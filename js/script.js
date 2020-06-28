75083930d54ccb1118263f982c9e949e8149f7ea
Denis Gorshkov <satrasmatrassatras@gmail.com>
2020-06-28 03:21:56 +0300

    feat: add open/close modals events

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
+// Form
+const messageForm = document.querySelector('.send-message-form');
+const messageFormShowButton = document.querySelector('.contacts-section .button');
+const messageFormCloseButton = messageForm.querySelector('.close-button');
+
+messageFormShowButton.addEventListener('click', event => {
+  event.preventDefault();
+  messageForm.classList.add('visible');
+})
+
+messageFormCloseButton.addEventListener('click', () => {
+  messageForm.classList.remove('visible');
+})
+
+// Map
+const mapModal = document.querySelector('.map-modal');
+const mapModalShowButton = document.querySelector('.contacts-section .contacts-map');
+const mapModalCloseButton = mapModal.querySelector('.close-button');
+
+mapModalShowButton.addEventListener('click', event => {
+  event.preventDefault();
+  mapModal.classList.add('visible');
+})
+mapModalCloseButton.addEventListener('click', () => {
+  mapModal.classList.remove('visible');
+})
+
 // Promo-slider
 const promoSliderButtons = document.querySelectorAll(".slider-controls-button");
 
